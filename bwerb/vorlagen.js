0f20d487c3f5a5587c4fe702d817b79270c1a42c
andre <andre.karyoti.schuro@gmail.com>
2023-09-07 21:56:09 +0200

    added bwerb jsarray axample

---
+++ bwerb/vorlagen.js
@@ -0,0 +1,73 @@
+let spieler = [
+    {
+        'personalData': [
+            {
+                'telNum': '',
+                'email': '',
+                'birth': '',
+                'marital': '',
+                'address': '',
+                'photoPath': ''
+            }
+        ],
+
+        'kompetenzen': [],
+
+        'experience': [
+            {
+                'job1': [
+                    {
+                        'length': '',
+                        'title': '',
+                        'employer': '',
+                        'location': '',
+                        'tasks': []
+                    }
+                ],
+
+                'job2': [
+                    {
+                        'length': '',
+                        'title': '',
+                        'employer': '',
+                        'location': '',
+                        'tasks': []
+                    }
+                ],
+            }
+        ],
+
+        'apprentice': [
+            {
+                'appr1': [
+                    {
+                        'length': '',
+                        'jobTitle': '',
+                        'employer': '',
+                        'location': '',
+                        'average': '',
+                    }
+                ],
+
+                'appr2': [
+                    {
+                        'length': '',
+                        'jobTitle': '',
+                        'employer': '',
+                        'location': '',
+                        'average': '',
+                    }
+                ],
+            }
+        ],
+
+        'languages': [
+            {
+                'lang1': '',
+                'lang2': '',
+                'lang3': ''
+            }
+        ]
+
+    }
+]
