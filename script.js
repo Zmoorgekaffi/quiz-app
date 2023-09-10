bf2165c8c5d423a448f7fc2431bf7726be38f902
andre <andre.karyoti.schuro@gmail.com>
2023-09-10 21:38:54 +0200

    viele sachen verbessert

--- script.js
+++ script.js
@@ -1,3 +1,73 @@
+let currentSection = 0;
+let currentQuestion = 0;
+
+let data = [
+    {
+        title: 'Persönliche Daten',
+        questions: [
+            {
+                question: 'Wie lautet dein Vor- und Nachname?',
+                content: '<div class="input-group w-50">\
+                                <input id="input-data" type="text" class="form-control text-center" name="name" placeholder="Vorname Nachname">\
+                            </div>'
+            },
+            {
+                question: 'Wie lautet deine E-Mail-Adresse?',
+                content: '<div class="input-group w-50">\
+                                <input id="input-data" type="email" class="form-control text-center" name="mail" placeholder="E-Mail-Adresse">\
+                            </div>'
+            },
+        ]
+    },
+    {
+        title: 'Kompetenzen',
+        questions: [
+            {
+                question: 'Was kannst du überhaupt?',
+                content: '<div class="input-group w-50">\
+                                <input id="input-data" type="text" class="form-control text-center" name="competence" placeholder="Deine Lieblingseigenschaft">\
+                            </div>'
+            },
+        ]
+    },
+]
+
+function loadData() {
+    document.getElementById('section-title').innerHTML = data[currentSection].title
+    document.getElementById('question').innerHTML = data[currentSection].questions[currentQuestion].question
+    document.getElementById('question-content').innerHTML = data[currentSection].questions[currentQuestion].content
+    document.getElementById('question-counter').innerHTML = "Frage " + (currentQuestion + 1) + " / " + data[currentSection].questions.length
+}
+
+function nextStep() {
+
+    if (currentQuestion + 1 === data[currentSection].questions.length) {
+        currentSection++;
+        currentQuestion = 0;
+    } else {
+        currentQuestion++;
+    }
+
+    save();
+
+    loadData();
+}
+
+function save() {
+    var currentData = JSON.parse(localStorage.getItem('data'));
+    currentData[document.getElementById('input-data').getAttribute('name')] = document.getElementById('input-data').value;
+    localStorage.setItem('data', JSON.stringify(currentData));
+}
+
+function init() {
+    loadData()
+    localStorage.setItem('data', JSON.stringify({}))
+}
+
+
+/*** Bis hier Klemi Bang */
+
+
 let cVProcess = [
     {
         'title': 'Persönliche Daten',
@@ -32,11 +102,19 @@ function loadPersDataInput() {
     display.innerHTML = ``;
     display.innerHTML += `${generateCard(cVProcess[0].title, cVProcess[0].question1)}`
     document.getElementById('if').innerHTML += `${generateInputAndButton()}`;
+    document.getElementById('next-btn').setAttribute('onclick', 'loadPersDataInput2()');
+}
+
+function loadPersDataInput2() {
+
 }
 
 function generateInputAndButton() {
     let html = /*html*/`
-    <input type="text" placeholder="Vorname Nachname"><button class="btn btn-secondary">Füge deinen Namen hinzu</button>
+                <div class="input-group w-50">
+                    <input type="text" class="form-control" placeholder="Vorname Nachname">
+                    <button class="btn btn-outline-secondary" type="button" id="button-addon2">Namen Hinzufügen</button>
+                </div>
     `;
     return html;
 }
@@ -45,7 +123,7 @@ function generateCard(cardTitle, cardQuestion) {
     let html = /*html*/`
     <div class="card-body">
         <span class="w-100 d-flex justify-content-center align-items-center me-margin-bottom-2"><h5 class="card-title display-5">${cardTitle}</h5></span>
-        <p class="card-text" style="text-align: center;">${cardQuestion}</p>
+        <p class="card-text me-flex-center" style="text-align: center;">${cardQuestion}</p>
     </div>
     <div class=" w-100 d-flex justify-content-center align-items-center" style="gap: 8px;" id="if">
 
